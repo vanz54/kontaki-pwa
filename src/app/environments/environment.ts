cb3d9904499497de5676020da56f65696bb07798
Tommaso Vanz <110528455+vanz54@users.noreply.github.com>
2023-12-21 18:07:28 +0100

    Removed my firebase app

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,14 +2,12 @@
 export const environment = {
   production: true,
   firebase: {
-  apiKey: "AIzaSyBHkwthDZe8KInu468Jxy2iqVCkbnKBioE",
-  authDomain: "kontaki-tv54.firebaseapp.com",
-  databaseURL: "https://kontaki-tv54-default-rtdb.firebaseio.com",
-  projectId: "kontaki-tv54",
-  storageBucket: "kontaki-tv54.appspot.com",
-  messagingSenderId: "553017460838",
-  appId: "1:553017460838:web:19e1ea35966fe21d1305a7"
+  apiKey: "XXXXXXXXX",
+  authDomain: "XXXXXXXXX",
+  databaseURL: "XXXXXXXXX",
+  projectId: "XXXXXXXXX",
+  storageBucket: "XXXXXXXXX",
+  messagingSenderId: "XXXXXXXXX",
+  appId: "XXXXXXXXX"
   }
 };
-
-// Used in app.module.ts
